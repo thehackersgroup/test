c03274ac569275f5d1b1f389d36a67c5b1cdadef
Your Name <you@example.com>
2016-09-17 07:15:56 +0200

    accel and baro

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-./convert_to_csv.py sensor-reading.json sensor-reading-acceleration.csv
+./convert_to_csv.py sensor-reading.json sensor-reading # the second argument is only a prefix
 
 '''
 
@@ -9,10 +9,17 @@ import os, sys, json
 
 data = []
 
-out_fh = open(sys.argv[2], 'w')
+out_fh_accel = open(sys.argv[2] + '-accel.csv', 'w')
+out_fh_accel.write(','.join([ 'date', 'x', 'y', 'z' ]))
+out_fh_accel.write('\n')
+
+out_fh_baro = open(sys.argv[2] + '-baro.csv', 'w')
+out_fh_baro.write(','.join([ 'date', 'pres', 'alt' ]))
+out_fh_baro.write('\n')
 
 tmp = ''
 bra = 0
+
 for l in open(sys.argv[1]).readlines():
     if l.startswith(']') or l.startswith('['):
         continue
@@ -30,6 +37,9 @@ for l in open(sys.argv[1]).readlines():
         d = json.loads(tmp.rstrip())
         
         if d['type'] == 'Accelerometer':
-            out_fh.write(','.join(map(str, [ d['date'], d['x'], d['y'], d['z'], ])) + '\n')
+            out_fh_accel.write(','.join(map(str, [ d['date'], d['x'], d['y'], d['z'], ])) + '\n')
+            
+        elif d['type'] == 'Barometer':
+            out_fh_baro.write(','.join(map(str, [ d['date'], d['pressure'], d['relativeAltitude'] ])) + '\n')
      
         tmp = ''
