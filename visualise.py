d4fe94afce13cfc45282e75e47188d002fba3228
thehackersgroup <dg2@gugelmann.com>
2016-09-17 19:17:44 +0200

    Added "is_activity" column to read acceleration file

--- visualise.py
+++ visualise.py
@@ -4,22 +4,21 @@ import pandas as pd
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
 
-def regularise_time(ts, xs):
-    t1 = np.arange(ts[0], ts[len(ts) - 1], 0.1)
-    x1 = np.interp(t1, ts, xs)
+#def regularise_time(ts, xs):
+#    t1 = np.arange(ts[0], ts[len(ts) - 1], 0.1)
+#    x1 = np.interp(t1, ts, xs)
+#
+#    return(x1, t1)
 
-    return(x1, t1)
+t = df_accel.index - df_accel.index[0]
+t = [_t.seconds+float(_t.microseconds)/1000000 for _t in t]
+x = df_accel['x']
+y = df_accel['y']
+z = df_accel['z']
 
-activity_t = df_accel.index
-t0 = df_accel.index - df_accel.index[0]
-t0 = [_t.seconds+float(_t.microseconds)/1000000 for _t in t0]
-x0 = df_accel['x']
-y0 = df_accel['y']
-z0 = df_accel['z']
-
-(x, t) = regularise_time(t0, x0)
-(y, t) = regularise_time(t0, y0)
-(z, t) = regularise_time(t0, z0)
+#(x, t) = regularise_time(t0, x0)
+#(y, t) = regularise_time(t0, y0)
+#(z, t) = regularise_time(t0, z0)
 
 abs_val = []
 for i in range(0, len(t)):
@@ -38,13 +37,14 @@ plt.subplot(2,1,2)
 ts.rolling(DATA[DATASET]['rolling_std_width']).mean().plot(style='k')
 
 # add the 20 day rolling variance:
-rollingVar = ts.rolling(DATA[DATASET]['rolling_std_width']).std().dropna()
+rollingVar = ts.rolling(DATA[DATASET]['rolling_std_width']).std().fillna(method='backfill')
 percentile_result = np.nanpercentile(rollingVar, DATA[DATASET]['percentile'])
 
 window_size, poly_order = 25, 3
 itp = interp1d(rollingVar.index, rollingVar.values, kind='linear')
 rollingVar_sg = savgol_filter(itp(rollingVar.index), window_size, poly_order)
 is_above = rollingVar_sg > percentile_result
+df_accel['is_activity'] = is_above
 
 plt.figure(2)
 rollingVar.plot(style='b')
@@ -52,4 +52,4 @@ plt.plot(rollingVar.index, is_above, color = 'y')
 plt.plot(rollingVar.index, rollingVar_sg, color = 'k')
 plt.axhline(y=percentile_result, color = 'r')
 
-plt.show()
+#plt.show()
