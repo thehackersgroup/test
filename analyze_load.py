885f05ef48aca5a5a418c1dc4a3c60495bd15ec1
Your Name <you@example.com>
2016-09-17 13:05:22 +0200

    fix

--- analyze_load.py
+++ analyze_load.py
@@ -3,7 +3,7 @@
 import pandas
 
 df_accel = pandas.DataFrame.from_csv(FILES_PREFIX + '-accel.csv')
-df_baro = pandas.DataFrame.from_csv(FILES_PREFIX + '-accel.csv')
+df_baro = pandas.DataFrame.from_csv(FILES_PREFIX + '-baro.csv')
 
 print 'df_accel'
 print df_accel.describe()
