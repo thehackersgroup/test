1f4e59cb53eaa3109e7c8ded3cb44d01d65de188
Your Name <you@example.com>
2016-09-17 17:07:27 +0200

    barometer detection

--- analyze_baro.py
+++ analyze_baro.py
@@ -2,12 +2,14 @@
 
 from scipy.interpolate import interp1d
 from pylab import *
+from scipy.signal import savgol_filter
+
 
 def detect_step(xs, threshold=5.0, width=10, smoothing = 0.7):
     from scipy.signal import find_peaks_cwt
-    xs_mean = mean(xs)
     detected_steps = zeros(len(xs))
     detected_steps_binary = zeros(len(xs))
+    detected_steps_height = zeros(len(xs))
     total_smoothing_weight = 0
     for center in arange(width, len(xs) - width):
         sum_before = 0
@@ -21,7 +23,7 @@ def detect_step(xs, threshold=5.0, width=10, smoothing = 0.7):
         for i in arange(center + width -1, center -1, -1):
             sum_from *= smoothing
             sum_from += xs[i]
-        detected_steps[center] = float(sum_from - sum_before) / total_smoothing_weight * 2
+        detected_steps[center] = float(sum_from - sum_before)
     
     current_max = 0
     current_max_index = 0
@@ -34,24 +36,29 @@ def detect_step(xs, threshold=5.0, width=10, smoothing = 0.7):
             # end of over threshold series if current_max != 0
             if current_max > 0:
                 detected_steps_binary[current_max_index] = 1
+                detected_steps_height[current_max_index] = mean([xs[j] for j in range(current_max_index + width/2, current_max_index+width)]) - mean([xs[j] for j in range(current_max_index - width, current_max_index-width/2)])
+                print detected_steps_height[current_max_index]
             elif current_max < 0:
                 detected_steps_binary[current_max_index] = -1
+                detected_steps_height[current_max_index] = mean([xs[j] for j in range(current_max_index + width/2, current_max_index+width)]) - mean([xs[j] for j in range(current_max_index - width, current_max_index-width/2)])
             current_max = 0
             current_max_index = 0
 
-    return detected_steps_binary, detected_steps
+    return detected_steps_binary, detected_steps_height
 
 ##Smoothing data
-#t = df_baro.index - df_baro.index[0]
-#t = [_t.seconds for _t in t]
-#df_baro['t'] = t
+t = df_baro.index - df_baro.index[0]
+t = [_t.seconds for _t in t]
+df_baro['t'] = t
 
-#window_size, poly_order = 11, 3
-#itp = interp1d(t, df_baro['alt'], kind='linear')
-#df_baro['alt_fil'] = savgol_filter(itp(t), window_size, poly_order)
+window_size, poly_order = 11, 3
+itp = interp1d(t, df_baro['alt'], kind='linear')
+df_baro['alt_fil'] = savgol_filter(itp(t), window_size, poly_order)
 
 
-df_baro['alt_steps'], df_baro['alt_steps_height'] = detect_step(df_baro['alt'])
-df_baro[['alt_steps', 'alt']].plot(figsize=[20,10]) # alt_steps_height
+df_baro['alt_steps'], df_baro['alt_steps_height'] = detect_step(df_baro['alt'], width=DATA[DATASET]['detect_step_width'])
+df_baro['alt_steps'] = 1 * df_baro['alt_steps']
+df_baro[['alt_steps', 'alt', 'alt_steps_height']].plot(figsize=[12,12]) # alt_steps_height
 plot(df_baro.index, -5 * ones(len(df_baro.index)), 'x')
-legend()
+plot(gt[['start_time']], -7 * ones(len(gt.index)), 'x')
+plot(gt[['end_time']], -7 * ones(len(gt.index)), 'o')
--- data_to_df.py
+++ data_to_df.py
@@ -2,23 +2,48 @@ import pandas as pd
 import glob, os
 
 _dfs=[]
-i=0
 for file in glob.glob("%s/*.Barometer" % CHALLENGE_NUMBER):
-	print file
-	_dfs.append(pd.read_json(file))
+    print file
+    _dfs.append(pd.read_json(file))
 
 df_baro = pd.concat(_dfs, axis=0)
 df_baro.columns = [u'date', u'pressure', u'alt', u'type']
 df_baro.index = df_baro['date']
+df_baro.sort_index(inplace=True)
+
+# filtering
+print 'before filtering'
+print 'df_baro: from:', df_baro.index[0]
+print 'df_baro: to:', df_baro.index[-1]
+df_baro = df_baro[df_baro.index >= DATA[DATASET]['start']]
+df_baro = df_baro[df_baro.index <= DATA[DATASET]['end']]
+print 'after filtering'
+print 'df_baro: from:', df_baro.index[0]
+print 'df_baro: to:', df_baro.index[-1]
+
 
 _dfs=[]
-i=0
 for file in glob.glob("%s/*.Accelerometer" % CHALLENGE_NUMBER):
-	print file
-	_dfs.append(pd.read_json(file))
+    print file
+    _dfs.append(pd.read_json(file))
 
 df_accel = pd.concat(_dfs, axis=0)
 df_accel.index = df_accel['date']
+df_accel.sort_index(inplace=True)
+
+# filtering
+print 'before filtering'
+print 'df_accel: from:', df_accel.index[0]
+print 'df_accel: to:', df_accel.index[-1]
+df_accel = df_accel[df_accel.index >= DATA[DATASET]['start']]
+df_accel = df_accel[df_accel.index <= DATA[DATASET]['end']]
+print 'after filtering'
+print 'df_accel: from:', df_accel.index[0]
+print 'df_accel: to:', df_accel.index[-1]
+
+gt = pd.read_json(glob.glob("%s/*.json" % CHALLENGE_NUMBER)[0])
+
+
 
 
 
