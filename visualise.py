1bce3f22f6e60620b1a743d393f4991e9ec9350a
thehackersgroup <dg2@gugelmann.com>
2016-09-17 18:55:35 +0200

    Updated visualise.py for activity detection

--- visualise.py
+++ visualise.py
@@ -26,7 +26,6 @@ for i in range(0, len(t)):
 
 # some sample data
 ts = pd.Series(abs_val, index=t)
-ts = ts.iloc[400000:]
 
 #plot the time series
 plt.figure(1)
@@ -35,18 +34,21 @@ ts.plot(style='k--')
 
 # calculate a 60 day rolling mean and plot
 plt.subplot(2,1,2)
-ts.rolling(30).mean().plot(style='k')
+ts.rolling(DATA[DATASET]['rolling_std_width']).mean().plot(style='k')
 
 # add the 20 day rolling variance:
-rollingVar = ts.rolling(30).std()
-percentiles = [10, 25, 50, 75, 90]
-percentile_result = np.nanpercentile(rollingVar, percentiles)
-print percentiles
-print percentile_result
+rollingVar = ts.rolling(DATA[DATASET]['rolling_std_width']).std().dropna()
+percentile_result = np.nanpercentile(rollingVar, DATA[DATASET]['percentile'])
+
+window_size, poly_order = 25, 3
+itp = interp1d(rollingVar.index, rollingVar.values, kind='linear')
+rollingVar_sg = savgol_filter(itp(rollingVar.index), window_size, poly_order)
+is_above = rollingVar_sg > percentile_result
 
 plt.figure(2)
 rollingVar.plot(style='b')
-for i in range(0,len(percentiles)):
-    plt.axhline(y=percentile_result[i], color = 'r')
+plt.plot(rollingVar.index, is_above, color = 'y')
+plt.plot(rollingVar.index, rollingVar_sg, color = 'k')
+plt.axhline(y=percentile_result, color = 'r')
 
 plt.show()
